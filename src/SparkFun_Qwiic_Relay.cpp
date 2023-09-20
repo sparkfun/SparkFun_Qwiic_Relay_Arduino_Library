74d0566236b7d889ee80d471eeb0e8a06bfc6f75
Elias Santistevan <elias.santistevan@sparkfun.com>
2023-09-20 11:18:46 -0600

    Formats the files

--- src/SparkFun_Qwiic_Relay.cpp
+++ src/SparkFun_Qwiic_Relay.cpp
@@ -1,9 +1,9 @@
 /*
   This is a library for both the Qwiic Single and Qwiic Quad Relay. It gives basic
-  functionality for turning on and off your relays and getting their statuses. 
+  functionality for turning on and off your relays and getting their statuses.
   By: Elias Santistevan
-  Date: July 2019 
-  License: This code is public domain but you buy me a beer if you use this and 
+  Date: July 2019
+  License: This code is public domain but you buy me a beer if you use this and
   we meet someday (Beerware license).
 
   Feel like supporting our work? Buy a board from SparkFun!
@@ -11,34 +11,36 @@
 
 #include "SparkFun_Qwiic_Relay.h"
 
-Qwiic_Relay::Qwiic_Relay( uint8_t address ){  _address = address; } //Constructor for I-squared-C
-
-bool Qwiic_Relay::begin( TwoWire &wirePort )
+Qwiic_Relay::Qwiic_Relay(uint8_t address)
 {
-  
-  _i2cPort = &wirePort;
+    _address = address;
+} // Constructor for I-squared-C
 
-  _i2cPort->beginTransmission(_address);
-  uint8_t _ret = _i2cPort->endTransmission();
-  if( !_ret )
-    return true; 
-  else 
-    return false; 
+bool Qwiic_Relay::begin(TwoWire &wirePort)
+{
 
+    _i2cPort = &wirePort;
+
+    _i2cPort->beginTransmission(_address);
+    uint8_t _ret = _i2cPort->endTransmission();
+    if (!_ret)
+        return true;
+    else
+        return false;
 }
 
 //****----THE FOLLOWING FIVE FUNCTIONS ARE TO BE USED WITH THE SPARKFUN SINGLE RELAY-----****
 
-// This function turns the single relay board on. 
+// This function turns the single relay board on.
 void Qwiic_Relay::turnRelayOn()
 {
-  _writeCommandOn(TURN_RELAY_ON);
+    _writeCommandOn(TURN_RELAY_ON);
 }
 
-// This function turns the single relay board off. 
-void Qwiic_Relay::turnRelayOff() 
+// This function turns the single relay board off.
+void Qwiic_Relay::turnRelayOff()
 {
-  _writeCommandOff(TURN_RELAY_OFF);
+    _writeCommandOff(TURN_RELAY_OFF);
 }
 
 // This function gives toggle functionality to the single relay. First the
@@ -46,90 +48,91 @@ void Qwiic_Relay::turnRelayOff()
 // of the relay: on ---> off or off ----> on.
 void Qwiic_Relay::toggleRelay()
 {
-  uint8_t status = _readCommand(MYSTATUS);
-  if (status == SING_RELAY_ON)
-    turnRelayOff();
-  else  
-    turnRelayOn();
+    uint8_t status = _readCommand(MYSTATUS);
+    if (status == SING_RELAY_ON)
+        turnRelayOff();
+    else
+        turnRelayOn();
 }
 
 // This function for the SparkFun Single Relay, gets the status of the relay:
 // whether on: 1 or off: 0;
 uint8_t Qwiic_Relay::getState()
 {
-  uint8_t status =  _readCommand(MYSTATUS);
-  return status; 
+    uint8_t status = _readCommand(MYSTATUS);
+    return status;
 }
 
 // This function gets the version number of the SparkFun Single Relay.
 float Qwiic_Relay::singleRelayVersion()
 {
-  float version = _readVersion(FIRMWARE_VERSION);
-  return(version);
+    float version = _readVersion(FIRMWARE_VERSION);
+    return (version);
 }
 
-
-//This function starts a slow PWM (1Hz) with a range from 0-100 so as this is the maximum PWM resolution for Zero-crossing SSR's at 50Hz
+// This function starts a slow PWM (1Hz) with a range from 0-100 so as this is the maximum PWM resolution for
+// Zero-crossing SSR's at 50Hz
 bool Qwiic_Relay::setSlowPWM(uint8_t relay, uint8_t pwmValue)
 {
-  return _writeAddress(RELAY_ONE_PWM + relay - 1, pwmValue);
+    return _writeAddress(RELAY_ONE_PWM + relay - 1, pwmValue);
 }
 
-//This function starts a slow PWM (1Hz) with a range from 0-100 so as this is the maximum PWM resolution for Zero-crossing SSR's at 50Hz
+// This function starts a slow PWM (1Hz) with a range from 0-100 so as this is the maximum PWM resolution for
+// Zero-crossing SSR's at 50Hz
 uint8_t Qwiic_Relay::getSlowPWM(uint8_t relay)
 {
-  return _readCommand(RELAY_ONE_PWM + relay - 1);
+    return _readCommand(RELAY_ONE_PWM + relay - 1);
 }
 
 //*****----THE FOLLOWING FUNCTIONS ARE TO BE USED WITH THE SPARKFUN QUAD RELAY------*****
- 
+
 // This function turns the given relay on.
 void Qwiic_Relay::turnRelayOn(uint8_t relay)
 {
-  _writeCommandOn(relay);
+    _writeCommandOn(relay);
 }
 
 // This function turns the given relay off.
 void Qwiic_Relay::turnRelayOff(uint8_t relay)
 {
-  _writeCommandOff(relay);
+    _writeCommandOff(relay);
 }
 
 // This function toggles the given relay. If the relay is on then it will turn
-// it off, and if it is off then it will turn it on. 
+// it off, and if it is off then it will turn it on.
 void Qwiic_Relay::toggleRelay(uint8_t relay)
 {
-  if (relay == RELAY_ONE)
-    _writeCommandToggle(TOGGLE_RELAY_ONE);
-  else if (relay == RELAY_TWO)
-    _writeCommandToggle(TOGGLE_RELAY_TWO);
-  else if (relay == RELAY_THREE)
-    _writeCommandToggle(TOGGLE_RELAY_THREE);
-  else if(relay == RELAY_FOUR)
-    _writeCommandToggle(TOGGLE_RELAY_FOUR);
-  else 
-    return;
+    if (relay == RELAY_ONE)
+        _writeCommandToggle(TOGGLE_RELAY_ONE);
+    else if (relay == RELAY_TWO)
+        _writeCommandToggle(TOGGLE_RELAY_TWO);
+    else if (relay == RELAY_THREE)
+        _writeCommandToggle(TOGGLE_RELAY_THREE);
+    else if (relay == RELAY_FOUR)
+        _writeCommandToggle(TOGGLE_RELAY_FOUR);
+    else
+        return;
 }
 
 // This function for the SparkFun Quad Relay, turns on all relays on the
-// board. 
+// board.
 void Qwiic_Relay::turnAllRelaysOn()
 {
-  _writeCommandOn(TURN_ALL_ON);
+    _writeCommandOn(TURN_ALL_ON);
 }
 
 // This function for the SparkFun Quad Relay, turns off all relays on the
-// board. 
+// board.
 void Qwiic_Relay::turnAllRelaysOff()
 {
-  _writeCommandOff(TURN_ALL_OFF);
+    _writeCommandOff(TURN_ALL_OFF);
 }
 
 // This function for the SparkFun Quad Relay, turns off all relays on the
-// board. 
+// board.
 void Qwiic_Relay::toggleAllRelays()
 {
-  _writeCommandOn(TOGGLE_ALL);
+    _writeCommandOn(TOGGLE_ALL);
 }
 
 // This function for the SparkFun Quad Relay, gets the status of the relay:
@@ -137,195 +140,217 @@ void Qwiic_Relay::toggleAllRelays()
 uint8_t Qwiic_Relay::getState(uint8_t relay)
 {
 
-  uint8_t status; 
-  if(relay == RELAY_ONE)
-    status =  _readCommand(RELAY_ONE_STATUS);
-  else if(relay == RELAY_TWO)
-    status =  _readCommand(RELAY_TWO_STATUS);
-  else if(relay == RELAY_THREE)
-    status =  _readCommand(RELAY_THREE_STATUS);
-  else if(relay == RELAY_FOUR)
-    status =  _readCommand(RELAY_FOUR_STATUS);
-  else
-    return INCORR_PARAM;
-  
-  if( status == QUAD_RELAY_ON ) // Relay status should be consistent
-    return 1; // Relay on
-  else
-    return QUAD_RELAY_OFF;
+    uint8_t status;
+    if (relay == RELAY_ONE)
+        status = _readCommand(RELAY_ONE_STATUS);
+    else if (relay == RELAY_TWO)
+        status = _readCommand(RELAY_TWO_STATUS);
+    else if (relay == RELAY_THREE)
+        status = _readCommand(RELAY_THREE_STATUS);
+    else if (relay == RELAY_FOUR)
+        status = _readCommand(RELAY_FOUR_STATUS);
+    else
+        return INCORR_PARAM;
+
+    if (status == QUAD_RELAY_ON) // Relay status should be consistent
+        return 1;                // Relay on
+    else
+        return QUAD_RELAY_OFF;
 }
 
 // This function changes the I-squared-C address of the Qwiic RFID. The address
 // is written to the memory location in EEPROM that determines its address.
-bool Qwiic_Relay::changeAddress(uint8_t newAddress) 
+bool Qwiic_Relay::changeAddress(uint8_t newAddress)
 {
 
-  if (newAddress < 0x07 || newAddress > 0x78) // Range of legal addresses
-        return false; 
+    if (newAddress < 0x07 || newAddress > 0x78) // Range of legal addresses
+        return false;
 
-  _i2cPort->beginTransmission( _address);  
-  _i2cPort->write(ADDRESS_LOCATION);  
-  _i2cPort->write(newAddress);  
+    _i2cPort->beginTransmission(_address);
+    _i2cPort->write(ADDRESS_LOCATION);
+    _i2cPort->write(newAddress);
 
-  if(!_i2cPort->endTransmission())
-    return true; 
-  else
-    return false; 
-
+    if (!_i2cPort->endTransmission())
+        return true;
+    else
+        return false;
 }
 
 // This function writes a value to an address
 bool Qwiic_Relay::_writeAddress(uint8_t addressToWrite, uint8_t value)
 {
-  _i2cPort->beginTransmission(_address); // Start communication.
-  _i2cPort->write(addressToWrite); // Toggle it on....
-  _i2cPort->write(value);
-  if (_i2cPort->endTransmission() != 0)
-  {
-    return false; //Transaction failed
-  } // End communcation.
-  else
-  {
-    return true;
-  }
-  
+    _i2cPort->beginTransmission(_address); // Start communication.
+    _i2cPort->write(addressToWrite);       // Toggle it on....
+    _i2cPort->write(value);
+    if (_i2cPort->endTransmission() != 0)
+    {
+        return false; // Transaction failed
+    }                 // End communcation.
+    else
+    {
+        return true;
+    }
 }
-// This function handles I-squared-C write commands for turning the relays on. 
+// This function handles I-squared-C write commands for turning the relays on.
 // The quad relay relies on the current state of the relay to determine whether
 // or not to turn the respective relay on (or off) and so the current state of
-// the relay is checked before attempting to send a command. 
+// the relay is checked before attempting to send a command.
 void Qwiic_Relay::_writeCommandOn(uint8_t _command)
 {
 
-  uint8_t _status; 
-  if (_command == RELAY_ONE){
-    _status = _readCommand(RELAY_ONE_STATUS);
-    if( _status == QUAD_RELAY_ON ){ // Is it on? Then....
-      return; // Do nothing....
+    uint8_t _status;
+    if (_command == RELAY_ONE)
+    {
+        _status = _readCommand(RELAY_ONE_STATUS);
+        if (_status == QUAD_RELAY_ON)
+        {           // Is it on? Then....
+            return; // Do nothing....
+        }
+        else
+        {                                          // Off?
+            _i2cPort->beginTransmission(_address); // Start communication.
+            _i2cPort->write(TOGGLE_RELAY_ONE);     // Toggle it on....
+            _i2cPort->endTransmission();           // End communcation.
+        }
     }
-    else { // Off?
-      _i2cPort->beginTransmission(_address); // Start communication.
-      _i2cPort->write(TOGGLE_RELAY_ONE); // Toggle it on....
-      _i2cPort->endTransmission(); // End communcation.
+    // Repeat for relay two....
+    else if (_command == RELAY_TWO)
+    {
+        _status = _readCommand(RELAY_TWO_STATUS);
+        if (_status == QUAD_RELAY_ON)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address);
+            _i2cPort->write(TOGGLE_RELAY_TWO);
+            _i2cPort->endTransmission();
+        }
     }
-  }
-  // Repeat for relay two....
-  else if (_command == RELAY_TWO){
-    _status = _readCommand(RELAY_TWO_STATUS);
-    if( _status == QUAD_RELAY_ON ){
-      return;
+    // Relay three..
+    else if (_command == RELAY_THREE)
+    {
+        _status = _readCommand(RELAY_THREE_STATUS);
+        if (_status == QUAD_RELAY_ON)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address);
+            _i2cPort->write(TOGGLE_RELAY_THREE);
+            _i2cPort->endTransmission();
+        }
     }
-    else {
-      _i2cPort->beginTransmission(_address);
-      _i2cPort->write(TOGGLE_RELAY_TWO); 
-      _i2cPort->endTransmission(); 
+    // Relay four....
+    else if (_command == RELAY_FOUR)
+    {
+        _status = _readCommand(RELAY_FOUR_STATUS);
+        if (_status == QUAD_RELAY_ON)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address);
+            _i2cPort->write(TOGGLE_RELAY_FOUR);
+            _i2cPort->endTransmission();
+        }
     }
-  }
-  // Relay three..
-  else if (_command == RELAY_THREE){
-    _status = _readCommand(RELAY_THREE_STATUS);
-    if( _status == QUAD_RELAY_ON ){
-      return;
+    // If it's not 1-4 then it must be for the single relay....
+    else
+    {
+        _i2cPort->beginTransmission(_address);
+        _i2cPort->write(_command);
+        _i2cPort->endTransmission();
     }
-    else {
-      _i2cPort->beginTransmission(_address); 
-      _i2cPort->write(TOGGLE_RELAY_THREE);
-      _i2cPort->endTransmission(); 
-    }
-  }
-  // Relay four.... 
-  else if (_command == RELAY_FOUR){
-    _status = _readCommand(RELAY_FOUR_STATUS);
-    if( _status == QUAD_RELAY_ON ){
-      return;
-    }
-    else {
-      _i2cPort->beginTransmission(_address);
-      _i2cPort->write(TOGGLE_RELAY_FOUR);
-      _i2cPort->endTransmission(); 
-    }
-  }
-  // If it's not 1-4 then it must be for the single relay....
-  else {
-    _i2cPort->beginTransmission(_address);
-    _i2cPort->write(_command);
-    _i2cPort->endTransmission(); 
-  }
-     
 }
 
 // This function handles I-squared-C write commands for toggling the relays from their
-// current state. If the relay is on then it will be turned off and vice versa. 
-void Qwiic_Relay::_writeCommandToggle(uint8_t _command){
+// current state. If the relay is on then it will be turned off and vice versa.
+void Qwiic_Relay::_writeCommandToggle(uint8_t _command)
+{
 
     _i2cPort->beginTransmission(_address);
     _i2cPort->write(_command);
-    _i2cPort->endTransmission(); 
-
+    _i2cPort->endTransmission();
 }
 
-// This function handles I-squared-C write commands for turning the relays off. 
+// This function handles I-squared-C write commands for turning the relays off.
 // The quad relay relies on the current state of the relay to determine whether
 // or not to turn the respective relay off (or on) and so the current state of
 // the relay is checked before attempting to toggle it.
 void Qwiic_Relay::_writeCommandOff(uint8_t _command)
 {
 
-  uint8_t _status;
-  if( _command == RELAY_ONE ){
-    _status = _readCommand(RELAY_ONE_STATUS);
-    if( _status == QUAD_RELAY_OFF ){ // Is the board off?
-      return; // Do nothing...
+    uint8_t _status;
+    if (_command == RELAY_ONE)
+    {
+        _status = _readCommand(RELAY_ONE_STATUS);
+        if (_status == QUAD_RELAY_OFF)
+        {           // Is the board off?
+            return; // Do nothing...
+        }
+        else
+        {                                          // Then it must be on...
+            _i2cPort->beginTransmission(_address); // Start communication.
+            _i2cPort->write(TOGGLE_RELAY_ONE);     // Toggle it off....
+            _i2cPort->endTransmission();           // End communcation.
+        }
     }
-    else { // Then it must be on...
-      _i2cPort->beginTransmission(_address); // Start communication.
-      _i2cPort->write(TOGGLE_RELAY_ONE); // Toggle it off....
-      _i2cPort->endTransmission(); // End communcation.
+    // Repeat for relay two....
+    else if (_command == RELAY_TWO)
+    {
+        _status = _readCommand(RELAY_TWO_STATUS);
+        if (_status == QUAD_RELAY_OFF)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address); // Start communication.
+            _i2cPort->write(TOGGLE_RELAY_TWO);
+            _i2cPort->endTransmission(); // End communcation.
+        }
     }
-  }
-  // Repeat for relay two....
-  else if( _command == RELAY_TWO ){
-    _status = _readCommand(RELAY_TWO_STATUS);
-    if( _status == QUAD_RELAY_OFF ){
-      return;
+    // Relay three...
+    else if (_command == RELAY_THREE)
+    {
+        _status = _readCommand(RELAY_THREE_STATUS);
+        if (_status == QUAD_RELAY_OFF)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address); // Start communication.
+            _i2cPort->write(TOGGLE_RELAY_THREE);
+            _i2cPort->endTransmission(); // End communcation.
+        }
     }
-    else {
-      _i2cPort->beginTransmission(_address); // Start communication.
-      _i2cPort->write(TOGGLE_RELAY_TWO); 
-      _i2cPort->endTransmission(); // End communcation.
+    // Relay four....
+    else if (_command == RELAY_FOUR)
+    {
+        _status = _readCommand(RELAY_FOUR_STATUS);
+        if (_status == QUAD_RELAY_OFF)
+        {
+            return;
+        }
+        else
+        {
+            _i2cPort->beginTransmission(_address); // Start communication.
+            _i2cPort->write(TOGGLE_RELAY_FOUR);
+            _i2cPort->endTransmission(); // End communcation.
+        }
     }
-  }
-  // Relay three...
-  else if( _command == RELAY_THREE ){
-    _status = _readCommand(RELAY_THREE_STATUS);
-    if( _status == QUAD_RELAY_OFF ){
-      return;
+    // If it's not 1-4 then it must be for the single relay...
+    else
+    {
+        _i2cPort->beginTransmission(_address);
+        _i2cPort->write(_command);
+        _i2cPort->endTransmission();
     }
-    else {
-      _i2cPort->beginTransmission(_address); // Start communication.
-      _i2cPort->write(TOGGLE_RELAY_THREE);
-      _i2cPort->endTransmission(); // End communcation.
-    }
-  }
-  // Relay four....
-  else if( _command == RELAY_FOUR ){
-    _status = _readCommand(RELAY_FOUR_STATUS);
-    if( _status == QUAD_RELAY_OFF ){
-      return;
-    }
-    else {
-      _i2cPort->beginTransmission(_address); // Start communication.
-      _i2cPort->write(TOGGLE_RELAY_FOUR);
-      _i2cPort->endTransmission(); // End communcation.
-    }
-  }
-  // If it's not 1-4 then it must be for the single relay...
-  else {
-    _i2cPort->beginTransmission(_address);
-    _i2cPort->write(_command);
-    _i2cPort->endTransmission(); 
-  }
-     
 }
 
 // This function requests information from the product with a simple
@@ -333,27 +358,24 @@ void Qwiic_Relay::_writeCommandOff(uint8_t _command)
 uint8_t Qwiic_Relay::_readCommand(uint8_t _command)
 {
 
-  _i2cPort->beginTransmission(_address); 
-  _i2cPort->write(_command);
-  _i2cPort->endTransmission();
+    _i2cPort->beginTransmission(_address);
+    _i2cPort->write(_command);
+    _i2cPort->endTransmission();
 
-  _i2cPort->requestFrom(_address, 1);  
-  uint8_t status = _i2cPort->read();
-  return(status);
-
+    _i2cPort->requestFrom(_address, 1);
+    uint8_t status = _i2cPort->read();
+    return (status);
 }
 
 // The function reads thee version number of the Single Quad Relay.
 float Qwiic_Relay::_readVersion(uint8_t _command)
 {
-  _i2cPort->beginTransmission(_address); 
-  _i2cPort->write(_command); 
-  _i2cPort->endTransmission();
+    _i2cPort->beginTransmission(_address);
+    _i2cPort->write(_command);
+    _i2cPort->endTransmission();
 
-  _i2cPort->requestFrom(_address, 2); 
-  float _versValue = _i2cPort->read();
-  _versValue += (float)_i2cPort->read() / 10.0 ;
-  return(_versValue);
-  
+    _i2cPort->requestFrom(_address, 2);
+    float _versValue = _i2cPort->read();
+    _versValue += (float)_i2cPort->read() / 10.0;
+    return (_versValue);
 }
-
--- src/SparkFun_Qwiic_Relay.h
+++ src/SparkFun_Qwiic_Relay.h
@@ -1,87 +1,88 @@
 #ifndef _SPARKFUN_QWIIC_RELAY_H_
-#define _SPARKFUN_QWIIC_RELAY_H_ 
+#define _SPARKFUN_QWIIC_RELAY_H_
 
-#include <Wire.h>
 #include <Arduino.h>
+#include <Wire.h>
+#include <stdint.h>
 
-enum SF_AVAILABLE_QUAD_RELAYS {
+enum SF_AVAILABLE_QUAD_RELAYS
+{
 
-  RELAY_ONE           = 0x01,
-  RELAY_TWO,
-  RELAY_THREE,
-  RELAY_FOUR,
-
+    RELAY_ONE = 0x01,
+    RELAY_TWO,
+    RELAY_THREE,
+    RELAY_FOUR,
 };
 
-enum SF_QUAD_RELAY_COMMANDS {
+enum SF_QUAD_RELAY_COMMANDS
+{
 
-  TOGGLE_RELAY_ONE    = 0x01,
-  TOGGLE_RELAY_TWO,
-  TOGGLE_RELAY_THREE,
-  TOGGLE_RELAY_FOUR,
-  RELAY_ONE_STATUS,
-  RELAY_TWO_STATUS,
-  RELAY_THREE_STATUS,
-  RELAY_FOUR_STATUS,
-  TURN_ALL_OFF        = 0xA,
-  TURN_ALL_ON,       
-  TOGGLE_ALL,
-  RELAY_ONE_PWM       = 0x10,
-  RELAY_TWO_PWM,
-  RELAY_THREE_PWM,
-  RELAY_FOUR_PWM
+    TOGGLE_RELAY_ONE = 0x01,
+    TOGGLE_RELAY_TWO,
+    TOGGLE_RELAY_THREE,
+    TOGGLE_RELAY_FOUR,
+    RELAY_ONE_STATUS,
+    RELAY_TWO_STATUS,
+    RELAY_THREE_STATUS,
+    RELAY_FOUR_STATUS,
+    TURN_ALL_OFF = 0xA,
+    TURN_ALL_ON,
+    TOGGLE_ALL,
+    RELAY_ONE_PWM = 0x10,
+    RELAY_TWO_PWM,
+    RELAY_THREE_PWM,
+    RELAY_FOUR_PWM
 };
 
-enum SF_QUAD_RELAY_STATUS {
+enum SF_QUAD_RELAY_STATUS
+{
 
-  QUAD_RELAY_OFF      = 0,
-  QUAD_RELAY_ON       = 15  
-
-};   
-
-enum SF_SINGLE_RELAY_COMMANDS {
-  
-  TURN_RELAY_OFF      = 0x00,
-  TURN_RELAY_ON,
-  FIRMWARE_VERSION    = 0x04,
-  MYSTATUS       
-
+    QUAD_RELAY_OFF = 0,
+    QUAD_RELAY_ON = 15
 };
 
-enum SF_SINGLE_RELAY_STATUS {
+enum SF_SINGLE_RELAY_COMMANDS
+{
 
-  SING_RELAY_OFF      = 0,
-  SING_RELAY_ON       = 1  
+    TURN_RELAY_OFF = 0x00,
+    TURN_RELAY_ON,
+    FIRMWARE_VERSION = 0x04,
+    MYSTATUS
+};
 
-};   
+enum SF_SINGLE_RELAY_STATUS
+{
 
-#define QUAD_DEFAULT_ADDRESS     0x6D
-#define QUAD_ALTERNATE_ADDRESS   0x6C
-#define SINGLE_DEFAULT_ADDRESS   0x18
+    SING_RELAY_OFF = 0,
+    SING_RELAY_ON = 1
+};
+
+#define QUAD_DEFAULT_ADDRESS 0x6D
+#define QUAD_ALTERNATE_ADDRESS 0x6C
+#define SINGLE_DEFAULT_ADDRESS 0x18
 #define SINGLE_ALTERNATE_ADDRESS 0x19
 
-#define QUAD_SSR_DEFAULT_ADDRESS     0x08
-#define QUAD_SSR_ALTERNATE_ADDRESS   0x09
-#define DUAL_SSR_DEFAULT_ADDRESS   0x0A
+#define QUAD_SSR_DEFAULT_ADDRESS 0x08
+#define QUAD_SSR_ALTERNATE_ADDRESS 0x09
+#define DUAL_SSR_DEFAULT_ADDRESS 0x0A
 #define DUAL_SSR_ALTERNATE_ADDRESS 0x0B
 
 #define ADDRESS_LOCATION 0xC7
-#define INCORR_PARAM     0xFF
+#define INCORR_PARAM 0xFF
 
 class Qwiic_Relay
-{  
+{
   public:
-
     Qwiic_Relay(uint8_t address); // I-squared-C Constructor
 
     bool begin(TwoWire &wirePort = Wire); // begin function
 
     //****----THE FOLLOWING FIVE FUNCTIONS ARE TO BE USED WITH THE SPARKFUN SINGLE RELAY-----****
-    
-    // This function turns the single relay board on. 
+
+    // This function turns the single relay board on.
     void turnRelayOn();
 
-    // This function turns the single relay board off. 
+    // This function turns the single relay board off.
     void turnRelayOff();
 
     // This function gives toggle functionality to the single relay. First the
@@ -96,35 +97,37 @@ class Qwiic_Relay
     // This function gets the version number of the SparkFun Single Relay.
     float singleRelayVersion();
 
-    //This function starts a slow PWM (1Hz) with a range from 0-100 as this is the maximum PWM resolution for Zero-crossing SSR's at 50Hz
+    // This function starts a slow PWM (1Hz) with a range from 0-100 as this is the maximum PWM resolution for
+    // Zero-crossing SSR's at 50Hz
     bool setSlowPWM(uint8_t relay, uint8_t pwmValue);
-    
-    //This function starts a slow PWM (1Hz) with a range from 0-100 as this is the maximum PWM resolution for Zero-crossing SSR's at 50Hz
+
+    // This function starts a slow PWM (1Hz) with a range from 0-100 as this is the maximum PWM resolution for
+    // Zero-crossing SSR's at 50Hz
     uint8_t getSlowPWM(uint8_t relay);
-    
+
     //*****----THE FOLLOWING FUNCTIONS ARE TO BE USED WITH THE SPARKFUN QUAD RELAY------*****
-    
+
     // This function turns the given relay on. While this also works for the
-    // SparkFun Single Relay, it is meant for the SparkFun Quad Relay. 
+    // SparkFun Single Relay, it is meant for the SparkFun Quad Relay.
     void turnRelayOn(uint8_t relay);
 
     // This function turns the given relay off.
     void turnRelayOff(uint8_t relay);
 
     // This function toggles the given relay. If the relay is on then it will turn
-    // it off, and if it is off then it will turn it on. 
+    // it off, and if it is off then it will turn it on.
     void toggleRelay(uint8_t relay);
 
     // This function for the SparkFun Quad Relay, turns on all relays on the
-    // board. 
+    // board.
     void turnAllRelaysOn();
 
     // This function for the SparkFun Quad Relay, turns off all relays on the
-    // board. 
+    // board.
     void turnAllRelaysOff();
 
     // This function for the SparkFun Quad Relay, turns off all relays on the
-    // board. 
+    // board.
     void toggleAllRelays();
 
     // This function for the SparkFun Quad Relay, gets the status of the relay:
@@ -136,19 +139,18 @@ class Qwiic_Relay
     bool changeAddress(uint8_t newAddress);
 
   private:
-
     uint8_t _address;
 
-    //This function writes a value to an address in the relay, used to set PWM values
+    // This function writes a value to an address in the relay, used to set PWM values
     bool _writeAddress(uint8_t address, uint8_t value);
 
-    // This function handles I-squared-C write commands for turning the relays on. 
+    // This function handles I-squared-C write commands for turning the relays on.
     // The quad relay relies on the current state of the relay to determine whether
     // or not to turn the respective relay on (or off) and so the current state of
-    // the relay is checked before attempting to send a command. 
+    // the relay is checked before attempting to send a command.
     void _writeCommandOn(uint8_t _command);
 
-    // This function handles I-squared-C write commands for turning the relays off. 
+    // This function handles I-squared-C write commands for turning the relays off.
     // The quad relay relies on the current state of the relay to determine whether
     // or not to turn the respective relay off (or on) and so the current state of
     // the relay is checked before attempting to toggle it.
